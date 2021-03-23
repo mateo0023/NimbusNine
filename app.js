9abf468d874b4f121405f33190ee908d2f29d62f
ThomasHaire <haire_thomas@yahoo.com>
2021-03-23 18:41:47 -0400

    installed ejs
    installed ejs and set the view engine to ejs. Added access to the public folder for images and css.

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
 
-app.get('*', (req, res) => {
-    res.send('404');
-});
+// Setting view engine to ejs
+app.set('view engine', 'ejs');
+
+// Allowing for access to the stylesheet
+app.use(express.static(__dirname + "/public"));
 
 app.get('/', (req, res) => {
-    res.send('Nimbus Nine');
+    res.render('home');
 });
 
 app.listen(3000, () => {
