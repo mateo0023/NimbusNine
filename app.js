30c9037a7ab04036cc3be0730f669596d437b18d
thomashaire <haire_thomas@yahoo.com>
2021-03-25 20:50:26 -0400

    exress app

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+// Setting view engine to ejs
+app.set('view engine', 'ejs');
+
+// Allowing for access to the stylesheet
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.get('/signup', (req, res) => {
+    res.render('signup');
+})
+
+app.listen(3000, () => {
+    console.log('Server is up and running');
+})
