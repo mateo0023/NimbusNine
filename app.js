453c8d1cf23474f0933feae1e5c211824b003cc7
thomashaire <haire_thomas@yahoo.com>
2021-03-31 21:43:23 -0400

    added global variables, connected flash, added mongoose, and express session

--- app.js
+++ app.js
@@ -1,24 +1,60 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const indexRoutes = require("./routes/index");
+const userRoutes = require("./routes/users");
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
+
+// Passport config
+require('./config/passport')(passport);
+
+// DB config
+const db = require("./config/keys").MongoURI;
+
+// Connect to Mongo
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Setting view engine to ejs
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Allowing for access to the stylesheet
 app.use(express.static(__dirname + "/public"));
 
-app.get('/', (req, res) => {
-    res.render('home');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Express session
+app.use(session({
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global vars
+app.use((req, res, next) => {
+  res.locals.currentUser = req.user;
+  res.locals.successMsg = req.flash('successMsg');
+  res.locals.errorMsg = req.flash('errorMsg');
+  res.locals.loginError = req.flash('error');
+  next();
 });
 
-app.get('/login', (req, res) => {
-    res.render('login');
-})
+// Routes
+app.use("/", indexRoutes);
+app.use("/users", userRoutes);
 
-app.get('/signup', (req, res) => {
-    res.render('signup');
-})
 
 app.listen(3000, () => {
-    console.log('Server is up and running');
-})
+  console.log("Server is up and running");
+});
