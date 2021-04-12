bcc7badbc479a944e1e661c212ff8b856e6709e2
Mateo Aberastury <mateo.aberastury@gmail.com>
2021-04-12 14:24:40 -0400

    Dashboard now gets the items passed to the EJS

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,23 @@ router.get("/", (req, res) => {
 
 // Dashboard view
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
-  res.render('dashboard/dashboard', {
-    userName: req.user.userName,
-  });
+  db.treenodes.find({ owner: req.user._id, name: '~' })
+    .sort({ isFolder: 1, name: 1, _id: 1 })
+    .toArray((err, items) => {
+      if (err) res.send(err);
+
+      if (!items || items.length === 0){
+        res.render('dashboard/dashboard', {
+          items: false,
+          userName: req.user.userName,
+        });
+      }
+
+      res.render('dashboard/dashboard', {
+        items: items,
+        userName: req.user.userName,
+      });
+    })
 })
 
 module.exports = router;
